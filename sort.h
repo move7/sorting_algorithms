eed060d022a21df4401186e3654ad4072b2ec0c0
move7 <addouni.fatima@gmail.com>
2023-07-24 16:40:37 -0700

    task3

--- sort.h
+++ sort.h
@@ -40,6 +40,7 @@ void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
+void lomuto_sort(int *array, size_t size, int left, int right);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
