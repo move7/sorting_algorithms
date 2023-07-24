601ab75a1a6924945338b47f92a17f98183befa7
move7 <addouni.fatima@gmail.com>
2023-07-24 16:22:28 -0700

    task2

---
+++ 2-selection_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Sort an array of integers in ascending order
+ *
+ * @array: An array.
+ * @size: The size.
+ *
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		size_t min_idx = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_idx])
+				min_idx = j;
+		}
+
+		if (min_idx != i)
+		{
+			int temp = array[i];
+			array[i] = array[min_idx];
+			array[min_idx] = temp;
+			print_array(array, size);
+		}
+	}
+}
+
