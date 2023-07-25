74cd909158e66a03039105038bb01aec008df2be
move7 <addouni.fatima@gmail.com>
2023-07-25 10:15:14 -0700

    task2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -17,7 +17,7 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; i++)
 	{
 		size_t min_idx = i;
-		
+
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_idx])
@@ -27,7 +27,7 @@ void selection_sort(int *array, size_t size)
 		if (min_idx != i)
 		{
 			int temp = array[i];
-			
+
 			array[i] = array[min_idx];
 			array[min_idx] = temp;
 			print_array(array, size);
